b7f99e379187f63fa1c6f3a55db1ee2bbdf93736
AMit <amitmakode007@gmail.com>
2022-07-21 13:50:26 +0530

    this is my first code to push

---
+++ mdb.py
@@ -0,0 +1,17 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://amitkumar:Betul3146@amit.6qzfjzj.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d= {
+    "name": "amit",
+    "surname": "Kumar",
+    "Email": "amit@theresource"
+}
+
+l = {"java": "c++", "javascript": "pyhton", "scala":"nodj"}
+
+md= client["mdb"]
+coll = md["test"]
+coll.insert_one(d)
+coll.insert_one(l)
